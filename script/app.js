6f07d3490ae5eb5100f0d6c62414a12e9e77046a
michael pantoja <mymikey209@gmail.com>
2023-12-07 14:36:36 -0800

    api call

--- script/app.js
+++ script/app.js
@@ -1,10 +1,10 @@
-GetData();
-
-function GetData(){
-    fetch("https://api.adviceslip.com/advice").then(
-        response => response.json()
-    ).then(
-        // data => console.log(data)
-        data => dataHere.innerText = data.slip.advice
-    )
+async function todayHere(){
+    // this line is getting the data from the url which is getting weather from the entered lot and lan
+ let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4');
+ // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
+ let data = await promise.json();
+//  console.log(data);
 }
+todayHere(); 
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216')
