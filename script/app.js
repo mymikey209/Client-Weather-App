a6b77e0f9c3f5fdc6808fd78018c552d2469f2e6
michael pantoja <mymikey209@gmail.com>
2023-12-08 09:06:09 -0800

    hard coded weather

--- script/app.js
+++ script/app.js
@@ -3,17 +3,7 @@ async function todayHere(today){
  let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4');
  // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
  let data = await promise.json();
-//  console.log(data);
-document. getElementById('todayHere').innerText = todayHere;
+console.log(data);
+// document.getElementById('todayHere').innerText = todayHere;
 }
 todayHere();
-// fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216')
-
-
-
-async function ApiCall(today){
-    const promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4')
-    const data = await promise.json();
-
-
-}
