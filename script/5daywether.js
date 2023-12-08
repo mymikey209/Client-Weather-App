870e83401969171d133e1b9d29af48e57be425e4
michael pantoja <mymikey209@gmail.com>
2023-12-07 17:44:03 -0800

    api changes

--- script/5daywether.js
+++ script/5daywether.js
@@ -1,6 +1,6 @@
 async function fiveDayHere(){
     let promise = await fetch('api.openweathermap.org/data/2.5/forecast?lat=37.797&lon=-121.216&appid=a83a8a3f14f7269a62f56270da542afd');
     let data = await promise.json();
-    console.log(data);
+    // console.log(data);
 }
 fiveDayHere();
--- script/app.js
+++ script/app.js
@@ -1,10 +1,19 @@
-async function todayHere(){
+async function todayHere(today){
     // this line is getting the data from the url which is getting weather from the entered lot and lan
  let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4');
  // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
  let data = await promise.json();
 //  console.log(data);
+document. getElementById('todayHere').innerText = todayHere;
 }
-todayHere(); 
-
+todayHere();
 // fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216')
+
+
+
+async function ApiCall(today){
+    const promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4')
+    const data = await promise.json();
+
+
+}
