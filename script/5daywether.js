b5d19ba2a462d30e4467be2586c1a330a57a5fbe
michael pantoja <mymikey209@gmail.com>
2023-12-08 20:44:28 -0800

    today weather and monday high and low done

--- script/5daywether.js
+++ script/5daywether.js
@@ -1,6 +1,42 @@
-// async function fiveDayHere(){
-//     let promise = await fetch('api.openweathermap.org/data/2.5/forecast?lat=37.797&lon=-121.216&appid=a83a8a3f14f7269a62f56270da542afd');
-//     let data = await promise.json();
-//     // console.log(data);
-// }
-// fiveDayHere();
+let monHighandLow = document.getElementById('monLowHere');
+let monChances = document.getElementById('monChancesHere');
+let monClouds = document.getElementById('monCloudsHere');
+let monWindSpeed = document.getElementById('monWindHere');
+let monSunRise = document.getElementById('monSunriseHere');
+let monSunSet = document.getElementById('monSunsetHere');
+let tueHighandLow = document.getElementById('tueLowHere');
+let tueChances = document.getElementById('tueChancesHere');
+let tueClouds = document.getElementById('tueCloudsHere');
+let tueWindSpeed = document.getElementById('tueWindHere');
+let tueSunRise = document.getElementById('tueSunriseHere');
+let tueSunSet = document.getElementById('tueSunsetHere');
+let wedHighandLow = document.getElementById('wedLowHere');
+let wedChances = document.getElementById('wedChancesHere');
+let wedClouds = document.getElementById('wedCloudsHere');
+let wedWindSpeed = document.getElementById('wedWindHere');
+let wedSunRise = document.getElementById('wedSunriseHere');
+let wedSunSet = document.getElementById('wedSunsetHere');
+let thuHighandLow = document.getElementById('thuLowHere');
+let thuChances = document.getElementById('thuChancesHere');
+let thuClouds = document.getElementById('thuCloudsHere');
+let thuWindSpeed = document.getElementById('thuWindHere');
+let thuSunRise = document.getElementById('thuSunriseHere');
+let thuSunSet = document.getElementById('thuSunsetHere');
+let friHighandLow = document.getElementById('friLowHere');
+let friChances = document.getElementById('friChancesHere');
+let friClouds = document.getElementById('friCloudsHere');
+let friWindSpeed = document.getElementById('friWindHere');
+let friSunRise = document.getElementById('friSunriseHere');
+let friSunSet = document.getElementById('friSunsetHere');
+
+
+
+async function getFiveDayWeather() {
+    let promise = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=37.797&lon=-121.216&appid=a83a8a3f14f7269a62f56270da542afd');
+
+
+    let data = await promise.json();
+    monHighandLow.innerText = ((data.list[18].main.temp_min -273.15)*1.8)+32 + '°F' + '/' + ((data.list[18].main.temp_max -273.15)*1.8)+32 + '°F'
+    console.log(data);
+}
+getFiveDayWeather();
--- script/app.js
+++ script/app.js
@@ -1,45 +1,81 @@
-async function today(today) {
+let feelslike = document.getElementById('feelsLike'); 
+let todayMin = document.getElementById('todayMin');
+let chance = document.getElementById("chanceHere");
+let cloudiness = document.getElementById("cloudinessHere");
+let windSpeed = document.getElementById("windSpeedHere");
+let sunrise = document.getElementById("sunriseHere");
+let sunset = document.getElementById("sunsetHere");
+
+
+async function getTodayWeather() {
     // this line is getting the data from the url which is getting weather from the entered lot and lan
     let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4&units=imperial');
-    // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
+    // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the parse into an object 
     let data = await promise.json();
-    let todayMin = data.main.temp_min + '°F' + '/' + data.main.temp_max + '°F'
     console.log(data.main.temp);
-    document.getElementById('todayMin').innerText = todayMin;
-}
-
-
-
-
-async function chanceHere(chance) {
-    // this line is getting the data from the url which is getting weather from the entered lot and lan
-    let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4');
-    // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
-    let data = await promise.json();
-    let chanceHere = data.main.rain
-    console.log(data)
-    document.getElementById('chanceHere').innerText = chanceHere;
-    if (chanceHere === undefined) {
-        document.getElementById('chanceHere').innerText = "No rain";
+    todayMin.innerText = data.main.temp_min + '°F' + '/' + data.main.temp_max + '°F';
+    chance.innerText = data.main.rain
+    if (data.main.rain === undefined) {
+        chance.innerText = "No rain";
     }
-}
-
-async function cloudinessHere(cloudiness) {
-    // this line is getting the data from the url which is getting weather from the entered lot and lan
-    let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4&units=imperial');
-    // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
-    let data = await promise.json();
-    let cloudinessHere = data.main.clouds
-    console.log(data.main.clouds);
-    document.getElementById('cloudinessHere').innerText = cloudiness;
-    if (cloudinessHere === undefined) {
-        document.getElementById('cloudinessHere').innerText = "No clouds";
+    cloudiness.innerText = data.main.clouds
+    if (data.main.rain === undefined) {
+        cloudiness.innerText = "No clouds"
     }
+    console.log(data);
+    windSpeed.innerText = data.wind.speed + 'mph'
+    sunrise.innerText =  new Date(data.sys.sunrise).getHours() + '' + "am"
+    sunset.innerText = new Date(data.sys.sunset).getHours() + '' + "pm"
+    console.log(data.sys.sunset);
+    console.log(new Date(1702082753).getHours());
+    feelslike.innerText = data.main.feels_like 
+    
 }
+getTodayWeather();
 
-today();
-chanceHere();
-cloudinessHere();
-windSpeedHere();
-sunriseHere();
-sunsetHere();
+
+
+
+
+// async function chanceHere(chance) {
+//     // this line is getting the data from the url which is getting weather from the entered lot and lan
+//     let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4');
+//     // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
+//     let data = await promise.json();
+//     let chanceHere = data.main.rain
+//     console.log(data)
+//     document.getElementById('chanceHere').innerText = chanceHere;
+//     if (chanceHere === undefined) {
+//         document.getElementById('chanceHere').innerText = "No rain";
+//     }
+// }
+
+// async function cloudinessHere(cloudiness) {
+//     // this line is getting the data from the url which is getting weather from the entered lot and lan
+//     let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4&units=imperial');
+//     // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
+//     let data = await promise.json();
+//     let cloudinessHere = data.main.clouds
+//     console.log(data.main.clouds);
+//     document.getElementById('cloudinessHere').innerText = cloudiness;
+//     if (cloudinessHere === undefined) {
+//         document.getElementById('cloudinessHere').innerText = "No clouds";
+//     }
+// }
+
+// async function windSpeedHere(windSpeed) {
+//     // this line is getting the data from the url which is getting weather from the entered lot and lan
+//     let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.797&lon=-121.216&appid=2d380a5e9e23c1b1a805cb1d2431b6b4&units=imperial');
+//     // this line is creating a new data and the await is telling it to wait and the promise is holding the api data the json is trying the promise into an object 
+//     let data = await promise.json();
+//     let windSpeedHere = data.wind.speed
+//     console.log(data.wind.speed);
+//     document.getElementById('windSpeedHere').innerText = windSpeed;
+// }
+
+// today();
+// chanceHere();
+// cloudinessHere();
+// windSpeedHere();
+// sunriseHere();
+// sunsetHere();
